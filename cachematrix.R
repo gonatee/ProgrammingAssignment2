e1c69714bbdc9ac8f80accf2038cb777d504044c
gonatee <gonatee.k@gmail.com>
2015-01-26 06:24:31 +0700

    Coursera Assignment 2
    Comment added.

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,12 @@
 ## functions do
 ## Write a short comment describing this function
 
+## The first function, makeCacheMatrix creates a list containing a function to
+## set the value of the Matrix
+## get the value of the Matrix
+## set the value of the inverse
+## get the value of the inverse
+
 makeCacheMatrix <- function(x = matrix()) {
   ## iMatrix = Inverted Matrix
   iMatrix <- NULL
@@ -18,6 +24,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+## The following function calculates the inverse of the "matrix" created with the above function. 
+## 1. checks to see if the inverted version has already been calculated
+##   If so - it gets the value from the cache and skips the computation
+##   Else - it calculates the inverted version of the data then sets the value in the cache via the setinverse function.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
